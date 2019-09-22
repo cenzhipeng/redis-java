2ef26180ef91d55b71412ae5c037a316f5c70463
cenzhipeng <cenzhipeng@aliyun.com>
2019-09-22 16:04:54 +0800

    docker compose environment init

--- src/test/java/com/cenzhipeng/redis/RedisServerTest.java
+++ src/test/java/com/cenzhipeng/redis/RedisServerTest.java
@@ -2,9 +2,12 @@ package com.cenzhipeng.redis;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+@Slf4j
 public class RedisServerTest {
     private static RedisClient redisClient;
     private static RedisServer redisServer;
@@ -12,12 +15,12 @@ public class RedisServerTest {
     @BeforeClass
     public static void initClient() {
         // start the embed redis server
-        redisServer = RedisServer.builder().build();
-        redisServer.start();
+//        redisServer = RedisServer.builder().build();
+//        redisServer.start();
         RedisURI uri = RedisURI.builder()
-                .withHost("localhost")
-                .withPassword("123456")
-                .withPort(6379)
+                .withHost("redis")
+//                .withPassword("123456")
+                .withPort(63791)
                 .withDatabase(0)
                 .build();
         redisClient = RedisClient.create(uri);
@@ -26,7 +29,12 @@ public class RedisServerTest {
     @AfterClass
     public static void releaseClient(){
         redisClient.shutdown();
-        redisServer.shutDown();
+//        redisServer.shutDown();
+    }
+
+    @Test
+    public void testInfo(){
+        log.info(redisClient.connect().sync().info());
     }
 
 }
